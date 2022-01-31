20a9dc536928559161c38421013500cf35744535
ahamedbasha55 <92045127+ahamedbasha55@users.noreply.github.com>
2022-01-31 15:18:41 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,7 +1,11 @@
-
+provider "aws" {
+    access_key = "AKIA52OBUQDIVP3VO4WP"
+    secret_key = "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"
+    region     = "ap-south-1"
+}
 
 resource "aws_instance" "web" {
-  ami           = "${data.aws_ami.ec2_instance.id}"
+  ami           = "ami-0af25d0df86db00c1"
   instance_type = "t2.micro"
 
   tags = {
