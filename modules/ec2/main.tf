a26fa02af63ddb73ba6d9d44615fcb1d9f1971bd
ahamedbasha55 <92045127+ahamedbasha55@users.noreply.github.com>
2022-01-31 15:17:21 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,15 +1,4 @@
-data "aws_caller_identity" "current" {}
 
-data "aws_ami" "ec2_instance" {
-  most_recent = true
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["${data.aws_caller_identity.current.account_id}"]
-}
 
 resource "aws_instance" "web" {
   ami           = "${data.aws_ami.ec2_instance.id}"
